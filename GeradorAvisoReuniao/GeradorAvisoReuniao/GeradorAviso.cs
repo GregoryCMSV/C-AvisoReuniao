666bc6a2bb9633b25fdc141986c3d2a4eab1ad20
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-03-22 15:03:20 -0300

    Criado projeto com templates de local e aviso
    Criado projeto com templates de local e aviso

---
+++ GeradorAvisoReuniao/GeradorAvisoReuniao/GeradorAviso.cs
@@ -0,0 +1,17 @@
+namespace GeradorAvisoReuniao
+{
+    public partial class GeradorAviso : Form
+    {
+        public GeradorAviso()
+        {
+            InitializeComponent();
+        }
+
+        private void GeradorAviso_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
